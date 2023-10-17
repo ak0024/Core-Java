4342e44bf0f6652c2b692b920e18b41456f01649
ak0024 <67861732+ak0024@users.noreply.github.com>
2023-10-17 23:00:24 +0530

    Update EmployeeServiceInterface.java

--- EmployeeServiceInterface.java
+++ EmployeeServiceInterface.java
@@ -7,11 +7,6 @@ import com.mindgate.domain.Employee;
 import com.mindgate.domain.Login;
 
 public interface EmployeeServiceInterafce {
-	boolean addNewEmployee(Employee employee);
-	Employee getEmployeeByEmployeeId(int employeeId);
-	List<Employee> getAllEmployee();
-	Employee updateEmployeeSalary(int employeeId,double newSalary);
-	boolean deleteEmployeeById(int employeeId);
-	Employee updateEmployeeName(int employeeId,String name);
+	
 	Login authenticateLogin(String userName,String password);
 }
